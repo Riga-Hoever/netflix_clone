25b417afb6e13b43812ea96e6dc385ba1f50ee60
Cin1983! <cinnymaria@gmail.com>
2021-01-07 23:43:03 +0100

    Update 7 januari 2021

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { FooterContainer } from "./components/container/footer_c";
-
 import "./App.css";
+import { FooterContainer } from "./components/container/footer";
+
 import Navbar from "./Navbar";
 import Home from "./Menu/Home";
 import Series from "./Menu/Series";
@@ -13,21 +13,23 @@ import History from "./Menu/History";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <div className="App">
+    <div className="App">
+        <FooterContainer/>
+      <Router>
+        <Switch>
           <Navbar />
+         
           <Route path="/" exact component={Home} />
           <Route path="/" exact component={Series} />
           <Route path="/" exact component={Films} />
           <Route path="/" exact component={Trending} />
           <Route path="/" exact component={MyList} />
           <Route path="/" exact component={History} />
-          <FooterContainer />
-        </div>
-      </Switch>
-    </Router>
+        </Switch>
+      </Router>
+    </div>
   );
 }
+
 
 export default App;
---
+++ src/components/container/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Footer from "../../footer/styles";
+import { footer } from "./footer";
+
+export function FooterContainer() {
+    return (
+        <Footer>
+           <Footer.Title>Questions Contact Us</Footer.Title> 
+       </Footer>
+   ) 
+}
