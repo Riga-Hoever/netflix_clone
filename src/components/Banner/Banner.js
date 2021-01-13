5e3013c40b2080350cdcb5aa084b1d2b5fd5a1cf
Cin1983! <cinnymaria@gmail.com>
2021-01-13 03:56:26 +0100

    update 3:56 13/1/2021

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,30 +1,43 @@
 import React, { useState, useEffect } from "react";
 import axios from "../requests/axios";
 import requests from "../requests/requests";
+import "./Banner.css";
 
 function Banner() {
-  const { movie, setMovie } = useState([]);
+  const [movie, setMovie ] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(requests.fetchNetflixOriginals);
+      const request = await axios.get(requests.fetchActionMovies);
       setMovie(
         request.data.results[
           Math.floor(Math.random() * request.data.results.length - 1)
         ]
       );
+      return request;
     }
     fetchData();
   }, []);
 
+  console.log(movie);
+ 
   return (
-    <header>
+    <header className="banner">
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+        )`,
+        backgroundPosition:"center center",
+      }}
       <div className="banner__contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
-        <h3>Title</h3>
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
+
+          <h1 className="banner__description">{movie?.overview}</h1>
         </div>
       </div>
     </header>
